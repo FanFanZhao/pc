a27c8a2e8ca9e6f2ce9fc2a847fb3c291c3f5b66
“用liu <769288260@qq.com>
2018-09-22 17:34:42 +0800

    header

---
+++ src/view/indexFooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="footer_wrap">
+        <div class="left">
+            <h2>DQ Global</h2>
+            <p>全球领先的数字资产交易平台</p>
+            <div class="icon_img">
+                <i class="iconfont icon-weibo"></i>
+            </div>
+        </div>
+
+    </div>
+</template>
