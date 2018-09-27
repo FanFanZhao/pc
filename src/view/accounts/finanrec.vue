3bd2bb2a4e7f766fcfc16cf4d80d86af48c34bae
sjw1995 <1543405619@qq.com>
2018-09-27 16:11:57 +0800

    sjw

--- src/view/accounts/finanrec.vue
+++ src/view/accounts/finanrec.vue
@@ -79,21 +79,45 @@ export default {
     data(){
         return {
             lang:'',
+            token:'',
             changeList:[]
         }
     },
+    created(){
+        this.token= localStorage.getItem('token') || '';
+    },
     methods:{
         getdata(){
-            this.$http({
-            url: this.$utils.laravel_api + 'wallet/list',
-            method:'post',
-            data:{}
-            }).then(res=>{
-                console.log(res)
+            console.log(123456)
+            var that = this;
+            $.ajax({
+                url: this.$utils.laravel_api + "wallet/list",
+                type: "POST",
+                dataType: "json",
+                async: true,
+                beforeSend: function beforeSend(request) {
+                   request.setRequestHeader("Authorization", that.token);
+                },
+                success: function success(data) {
+                console.log(data)
+                if (data.type == 'ok') {
+                   
+
+                } else if (data.type == '999') {
+                    
+                }
+                }
+            });
+            // this.$http({
+            // url: this.$utils.laravel_api + 'wallet/list',
+            // method:'post',
+            // data:{}
+            // }).then(res=>{
+            //     console.log(res)
  
-            }).catch(error=>{
-                console.log(error)
-            })
+            // }).catch(error=>{
+            //     console.log(error)
+            // })
         }
     },
     mounted(){
