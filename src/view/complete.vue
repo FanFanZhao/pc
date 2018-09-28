db9bce8a3ac6e1b7030ab33c1360fa058157dd92
e <769288260@qq.com>
2018-09-28 15:15:12 +0800

    complete

---
+++ src/view/complete.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="detail">
+        <div class="title fColor1">
+            <div class="inblock">
+                <span>全站交易</span>
+                <!-- <span class="ft12 curPer ml10 baseColor" @click="newData">刷新</span> -->
+            </div>
+            <div class="tab_title fr ft12 clear hide">
+                <span v-for="(way,index) in wayList" :class="{'active': index == isChoosed}" @click="wayChoosed(index)">{{way}}</span>
+            </div>
+        </div>
+        <div class="content">
+            <ul class="list-title fColor2 ft12 clear">
+                <li class="fl w12">时间</li>
+                <li class="fl w12">价格</li>
+                <li class="fl w12">交易量</li>
+            </ul>
+            <div class="container scroll" v-if="deList.length>0">
+                <ul v-for="itm in deList" class="list-item fColor1 ft12">
+                    <li class="clear">
+                        <span class="fl w12">{{itm.time}}</span>
+                        <span class="fl w14">{{itm.price}}</span>
+                        <span class="fl">{{itm.number}}</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="no_data tc" v-if="deList.length<=0">
+                <img src="../assets/images/nodata.png" alt="">
+                <p class="fColor2 ft18">暂无数据</p>   
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"detail",
+    data (){
+        return{
+            address:'',
+            isChoosed:2,
+            new:false,
+            wayList:["买入","卖出","全部"],
+            deList:[]
+        }
+    },
+    created(){
+        this.address = localStorage.getItem('address') || '';
+    },
+    methods:{
+        wayChoosed(index){
+            this.isChoosed=index;
+        },
+         //全站交易记录
+        complete(legals_id,currencys_id){
+            this.$http({
+                        url: '/api/'+'transaction/deal',
+                        method:'post',
+                        data:{
+                            legal_id:legals_id,
+                            currency_id:currencys_id
+                        },  
+                        headers: {'Authorization':  localStorage.getItem('token')},    
+                    }).then(res=>{
+                        console.log(res ,222)
+                        // layer.close(i);
+                        if(res.data.type == "ok"){
+                           this.deList = res.data.message.complete;
+                        }else{
+                            layer.msg(res.data.message)
+                        }
+                    }).catch(error=>{
+                        // console.log(error)
+                    })
+        }
+    },
+    mounted(){
+         var that = this;
+          eventBus.$on('toTrade0', function (data0) {
+                that.complete(data0.legal_id,data0.currency_id)
+          });
+           eventBus.$on('toTrade', function (data0) {
+                that.complete(data0.legal_id,data0.currency_id)
+          })
+    }
+
+    
+}
+</script>
+<style scoped>
+.title{height: 48px;line-height: 46px;padding: 0 40px 0 30px;background-color: #181b2a;}
+.tab_title{display: inline-block;line-height: 46px;height: 46px;}
+.tab_title span{cursor: pointer;}
+.tab_title span:not(:last-child) {margin-right: 40px;}
+.content{padding: 0 40px 0 30px;height: 330px;}
+.list-title{line-height: 40px; border-bottom: 1px solid #303b4b;height: 40px;}
+.list-title li{
+    width: 33%;
+    text-align: center;
+}
+.no_data{padding: 50px 0;}
+.container{height: 260px;overflow: auto;}
+.list-item li{line-height: 30px;}
+.list-item li span{display: inline-block; float: left; width: 33.3%;text-align: center;}
+
+.list-item li:hover{background-color: #2b3648}
+.list-item li span.green{color: #55a067}
+</style>
+
+
